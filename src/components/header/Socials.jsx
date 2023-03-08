0279b9973bdd54efe48c73c043d36f2a885620e8
Shah Zeb <zeb00002@algonquinlive.com>
2023-03-08 02:16:31 -0500

    Header done along with CSS and media queries

---
+++ src/components/header/Socials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const Socials = () => {
+  return (
+    <div className="socials">
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin /> </a>
+        <a href="https://github.com" target="_blank"> < FaGithub /></a>
+        
+       
+    </div>
+  )
+}
+
+export default Socials
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import Socials from './Socials'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Shah Zeb</h1>
+        <h5 className="text-light">Web Developer</h5>
+        <CTA />
+        < Socials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
